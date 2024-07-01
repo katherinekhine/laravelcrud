4e057c618ce870e1ec19544fc7901566ae613c07
katherinekhine <khaingkatherine@gmail.com>
2024-07-01 12:21:42 +0630

    update creat.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <h1 class="text-danger">Hello World</h1>
+    <div class="bg-dark py-2">
+        <h3 class="text-white text-center">Simple Laravel CRUD</h3>
+    </div>
+    <div class="roe d-flex justify-content-center">
+        <div class="col-md-10">
+            <div class="card border-0 shadow-lg my-3">
+                <div class="card-header">
+                    <h3>Create Product</h3>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control form-control-lg" placeholder="Name" name="name">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
